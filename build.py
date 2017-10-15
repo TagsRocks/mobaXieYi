708fcb72e6162ac3089f45494a006bcebcb485ab
li yong <liyonghelpme@gmail.com>
2017-10-15 23:37:42 +0800

    错误到名字空间 windows 版本的构建 google framework3

--- build.py
+++ build.py
@@ -80,7 +80,7 @@ def getModuleID(modName):
 
 
 f1 = os.listdir('protos')
-#f1 = ["CommonProtoc.proto", "MCopyProtoc.proto"]
+#f1 = ["CommonProtoc.proto", "McpProtoc.proto"]
 allProtos = []
 for i in f1:
 	if i.find('.proto') != -1  and i.find("dump") == -1 and i.find('.swp') == -1:
@@ -129,115 +129,129 @@ print "CMDIs"
 fullCMD = 'mono ProtoGen.exe %s protos/google/protobuf/csharp_options.proto protos/google/protobuf/descriptor.proto --proto_path=protos' % (comStr)
 print fullCMD
 #print comStr
-
 os.system(fullCMD)
 
-if os.path.exists("temp"):
-	#os.system("rm -r temp")
+
+def Main():
+	if os.path.exists("temp"):
+		#os.system("rm -r temp")
+		shutil.rmtree("temp")
+	#os.system('cp -r protos temp')
+
+	shutil.copytree("protos", "temp")
+
+	allF = os.listdir('temp')
+	csharpRep = '''
+	import "google/protobuf/csharp_options.proto";
+	option (google.protobuf.csharp_file_options).namespace = "MyLib";
+	option (google.protobuf.csharp_file_options).umbrella_classname = "CommonProtoc";
+	'''
+	newoption = '''
+	option optimize_for = LITE_RUNTIME;
+	'''
+
+	comStr2 = ''
+	for p in allF:
+	    if p.find('.dump') == -1 and p.find('.swp') == -1 and p.endswith('.proto'):
+	        name = os.path.join('temp', p)
+	        if not os.path.isdir(name):
+	            con = open(name).readlines()
+	            ncon = ''
+	            for l in con:
+	                #if l.find('csharp') == -1 and l.find('optimize_for') == -1:
+	                    ncon += l
+	                    
+	            #con = con.replace(csharpRep, '')
+	            f = open(name, 'w')
+	            f.write(ncon)
+	            f.close()
+	            comStr2 += name+" "
+
+	print 'cmdstr'
+	print comStr2
 	shutil.rmtree("temp")
-#os.system('copy -r protos temp')
-
-shutil.copytree("protos", "temp")
-
-allF = os.listdir('temp')
-csharpRep = '''
-import "google/protobuf/csharp_options.proto";
-option (google.protobuf.csharp_file_options).namespace = "MyLib";
-option (google.protobuf.csharp_file_options).umbrella_classname = "CommonProtoc";
-'''
-newoption = '''
-option optimize_for = LITE_RUNTIME;
-'''
-
-comStr2 = ''
-for p in allF:
-    if p.find('.dump') == -1 and p.find('.swp') == -1 and p.endswith('.proto'):
-        name = os.path.join('temp', p)
-        if not os.path.isdir(name):
-            con = open(name).readlines()
-            ncon = ''
-            for l in con:
-                #if l.find('csharp') == -1 and l.find('optimize_for') == -1:
-                    ncon += l
-                    
-            #con = con.replace(csharpRep, '')
-            f = open(name, 'w')
-            f.write(ncon)
-            f.close()
-            comStr2 += name+" "
-
-print 'cmdstr'
-print comStr2
-shutil.rmtree("temp")
-	
-	
-	
 		
-f = os.listdir('.')
-delFile = [
-'Util2.cs',
-'Util3.cs',
-'CSharpOptions.cs',
-'DescriptorProtoFile.cs',
-]
-for i in f:
-	if i.find('.cs') != -1 :
-		if i in delFile:
-			os.remove(i)
-		else:
-			#os.system('mv %s cs/%s' % (i, i))
-			shutil.move(i, "cs/%s" % (i))
+		
+		
+			
+	f = os.listdir('.')
+	delFile = [
+	'Util2.cs',
+	'Util3.cs',
+	'CSharpOptions.cs',
+	'DescriptorProtoFile.cs',
+	]
+	for i in f:
+		#if i.find('.cs') != -1 and i.find(".csproj") == -1 :
+		if i.endswith(".cs"):
+			if i in delFile:
+				os.remove(i)
+			else:
+				#os.system('mv %s cs/%s' % (i, i))
+				shutil.move(i, "cs/%s" % (i))
 
-f = open('nameMap.json', 'w')
-f.write(json.dumps(msgMap, indent=4, sort_value=True, separators=(', ', ': ')))
-f.close()
-
-
-import GenUtilFile
-GenUtilFile.GenUtil(msgMap.values())
-
-os.system('cp UpdateSln.py cs/')
-os.system('cp protoDll.csproj cs/')
-os.system('cp Google.ProtocolBuffersLite.dll cs/')
-
-os.chdir('cs')
-os.system('python UpdateSln.py')
-os.system('xbuild new.csproj')
-os.chdir('..')
-
-'''
-#U3D = '/Users/liyong/Desktop/allUnity/Assets/Plugins'
-U3D = '../trunk/Assets/Plugins'
-os.system('cp cs/obj/Debug/protoDll.dll %s' % (U3D))
-
-#U3D = '/Users/liyong/Desktop/allUnity/Assets/scripts/util'
-U3D = '../trunk/Assets/scripts/util'
-os.system('cp Util2.cs %s' % (U3D))
+	f = open('nameMap.json', 'w')
+	f.write(json.dumps(msgMap, indent=4, sort_value=True, separators=(', ', ': ')))
+	f.close()
 
 
-#U3D = '/Users/liyong/Desktop/allUnity/Assets/Resources'
-U3D = '../trunk/Assets/Resources'
-os.system('cp nameMap.json %s' % (U3D))
+	import GenUtilFile
+	GenUtilFile.GenUtil(msgMap.values())
 
-#Server = '/Users/liyong/Projects/SocketServer/packages'
-Server = '../trunk/SocketServer1/packages'
-os.system('cp cs/obj/Debug/protoDll.dll %s' % (Server))
+	#os.system('cp UpdateSln.py cs/')
+	shutil.copy2("UpdateSln.py", "cs/")
+	shutil.copy2("protoDll.csproj ", "cs/")
+	shutil.copy2("Google.ProtocolBuffersLite.dll", "cs/")
+	#os.system('cp protoDll.csproj cs/')
+	#os.system('cp Google.ProtocolBuffersLite.dll cs/')
+	#os.system("cp protoDll2.csproj protoDll.csproj")
 
-#Server2 = '/Users/liyong/Projects/SocketServer/SocketServer/bin/Debug'
-Server2 = '../trunk/SocketServer1/SocketServer/bin/Debug'
-os.system('cp nameMap.json %s' % (Server2))
-os.system('cp cs/obj/Debug/protoDll.dll %s' % (Server2))
+	os.chdir('cs')
+	os.system('python UpdateSln.py')
+	os.system('xbuild new.csproj')
+	os.chdir('..')
 
 
-f = open('Util2.cs', 'r')
-con = f.read();
-con = con.replace('using UnityEngine;', '')
-nf = open('Util3.cs', 'w')
-nf.write(con)
-nf.close()
+	#U3D = '/Users/liyong/Desktop/allUnity/Assets/Plugins'
+	U3D = '../smallGameClient/Assets/Plugins'
+	#os.system('cp cs/obj/Debug/protoDll.dll %s' % (U3D))
+	shutil.copy2("cs/obj/Debug/protoDll.dll", U3D)
 
-#Server3 = '/Users/liyong/Projects/SocketServer/SocketServer/Common/Util2.cs'
-Server3 = '../trunk/SocketServer1/SocketServer/Common/Util2.cs'
-os.system('cp Util3.cs %s' % (Server3))
-'''
+	#U3D = '/Users/liyong/Desktop/allUnity/Assets/scripts/util'
+	U3D = '../smallGameClient/Assets/scripts/util'
+	#os.system('cp Util2.cs %s' % (U3D))
+	shutil.copy2("Util2.cs", U3D)
 
+
+	#U3D = '/Users/liyong/Desktop/allUnity/Assets/Resources'
+	U3D = '../smallGameClient/Assets/Resources'
+	#os.system('cp nameMap.json %s' % (U3D))
+	shutil.copy2("nameMap.json", U3D)
+
+	#Server = '/Users/liyong/Projects/SocketServer/packages'
+	Server = '../tankServer/packages'
+	#os.system('cp cs/obj/Debug/protoDll.dll %s' % (Server))
+	shutil.copy2("cs/obj/Debug/protoDll.dll", Server)
+
+	#Server2 = '/Users/liyong/Projects/SocketServer/SocketServer/bin/Debug'
+	Server2 = '../tankServer/SocketServer/bin/Debug'
+	#os.system('cp nameMap.json %s' % (Server2))
+	shutil.copy2("nameMap.json", Server2)
+	#os.system('cp cs/obj/Debug/protoDll.dll %s' % (Server2))
+	shutil.copy2("cs/obj/Debug/protoDll.dll", Server2)
+
+
+	f = open('Util2.cs', 'r')
+	con = f.read();
+	con = con.replace('using UnityEngine;', '')
+	nf = open('Util3.cs', 'w')
+	nf.write(con)
+	nf.close()
+
+	#Server3 = '/Users/liyong/Projects/SocketServer/SocketServer/Common/Util2.cs'
+	Server3 = '../tankServer/SocketServer/Common/Util2.cs'
+	#os.system('cp Util3.cs %s' % (Server3))
+	shutil.copy2("Util3.cs", Server3)
+
+
+Main()
